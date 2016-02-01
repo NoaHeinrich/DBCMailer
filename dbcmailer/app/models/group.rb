1b3eb97c7b220e24c1de5a3b0022a445e18f2b58
Noah Heinrich <noahheinrich@gmail.com>
2016-02-01 15:52:35 -0600

    Write models and associations

--- dbcmailer/app/models/group.rb
+++ dbcmailer/app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ActiveRecord::Base
+  has_many :enrollments
+  has_many :joined_users, through: :enrollments, source: :user
+  belongs_to :admin, class_name: "User"
+  has_many :events
 end
